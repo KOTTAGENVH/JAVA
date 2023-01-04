02be7d9e2b7356156d93c7446901fcb0b530ff30
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2023-01-04 20:47:06 +0530

    Create Test.java

---
+++ OOP/Lab06-Inheritence/Q3/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+public static void main(String[]args) {
+	//Student s1 = new Student("NOWEN","STU123","Pannipitiya");
+	
+	//System.out.println("Name: "+s1.name+"\nDitNo : "+s1.ditno+"\nAddress : "+s1.address);
+Student s1 = new Student();
+s1.setName("Nimal");
+s1.setDitno("IT567");
+s1.setAddress("colombo");
+System.out.println("Name : "+s1.getName()+"\nDitNo : "+s1.getDitno() + "\nAddress : "+s1.getAddress());
+
+}
+}
