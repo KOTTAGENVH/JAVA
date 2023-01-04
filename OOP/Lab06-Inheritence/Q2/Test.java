89ae038626198ca604105f04e454ea31d44fda4e
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2023-01-04 20:43:36 +0530

    Create Test.java

---
+++ OOP/Lab06-Inheritence/Q2/Test.java
@@ -0,0 +1,8 @@
+
+public class Test {
+public static void main(String[]args) {
+	Student s1 = new Student("NOWEN","STU123","Pannipitiya");
+	
+	System.out.println("Name: "+s1.name+"\nDitNo : "+s1.ditno+"\nAddress : "+s1.address);
+}
+}
