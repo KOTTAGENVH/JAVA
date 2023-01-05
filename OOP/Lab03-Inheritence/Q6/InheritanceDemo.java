30c96f5b6bdc9c5314227b39bef5af7fc46761eb
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2023-01-05 10:02:32 +0530

    Create InheritanceDemo.java

---
+++ OOP/Lab03-Inheritence/Q6/InheritanceDemo.java
@@ -0,0 +1,10 @@
+public class InheritanceDemo {
+ public static void main(String[]args) {
+	 
+ Person p1 = new Person("kamal", "Colombo");
+ p1.showDetails();
+ 
+ Student s1=new Student("Amali","Kaluthara","IT678");
+ s1.showDetails();
+ }
+}
